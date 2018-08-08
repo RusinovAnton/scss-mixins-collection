dbdda25b6b88124e96fcd04f10d7750f0cc7e842
Rusinov Anton <rusinovantondev@gmail.com>
2018-08-08 23:41:05 +0300

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass');
 
-gulp.task('css', function(){
-    gulp.src('./test/scss/style.scss')
+function css() {
+    return gulp
+        .src('./test/scss/style.scss')
         .pipe(sass())
         .pipe(gulp.dest('./test/css'));
-});
+}
 
-gulp.task('test', ['css']);
-
-gulp.task('default', ['css']);
+gulp.task('test', css);
